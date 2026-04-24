c9c3b6d1b0bca7cd5ebeb33f82e12e347d6a2fd5
pschatzmann <phil.schatzmann@gmail.com>
2026-04-24 08:03:43 +0000

    Deploying to doxygen from @ pschatzmann/ESP32S3-h264@530b5b7d9a2d7c94ec0f3133599ef9085ecf5872 🚀

--- search/defines_5.js
+++ search/defines_5.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['h264_5fdefault_5fallocator_0',['H264_DEFAULT_ALLOCATOR',['../H264Config_8h.html#a20e1dc8d822cad235e0ab211345d8fdd',1,'H264Config.h']]],
-  ['h264bsd_5fcfg_5fdefault_1',['H264BSD_CFG_DEFAULT',['../h264bsd__decoder_8h.html#aa873bfb1ff2b219e431e396049f00b44',1,'h264bsd_decoder.h']]],
-  ['h264dec_5fopt_2',['H264DEC_OPT',['../basetype_8h.html#a3ae3772ab1debfa7dc16f7ba1d534f5a',1,'basetype.h']]]
+  ['h264bsd_5fcfg_5fdefault_0',['H264BSD_CFG_DEFAULT',['../h264bsd__decoder_8h.html#aa873bfb1ff2b219e431e396049f00b44',1,'h264bsd_decoder.h']]],
+  ['h264dec_5fopt_1',['H264DEC_OPT',['../basetype_8h.html#a3ae3772ab1debfa7dc16f7ba1d534f5a',1,'basetype.h']]]
 ];
